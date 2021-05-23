621d2f4a55639ce3f1432a22b3d23d1470fb723a
tealmatrix <tealmatrix>
2021-05-23 19:54:39 -0400

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,229 @@
+<?php require_once("../resources/config.php"); ?>
+<?php include(TEMPLATE_FRONT . DS . "header.php") ?>
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+        
+
+            <div class="col-md-12">
+
+                <div class="row carousel-holder">
+
+                    <div class="col-xlg-*">
+                        
+                        <img src="../public_html/assets/images/track.png" alt="pixar cars racetrack">
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="../public_html/assets/images/dochudson.png" alt="doc hudson">
+                            <div class="caption">
+                                <h4 class="pull-right">Doctor</h4>
+                                <h4><a href="product.html">Doc Hudson</a>
+                                </h4>
+                                <p>Good guy . Retired Dr & local judge<a target="_blank" href="https://pixar.fandom.com/wiki/Doc_Hudson"> pixar.fandom.com/wiki/Doc_Hudson</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">98 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="./assets/images/filmore.png" alt="cars filmore">
+                            <div class="caption">
+                                <h4 class="pull-right">1960</h4>
+                                <h4><a href="#">Filmore</a>
+                                </h4>
+                                <p>Hippie van <a target="_blank" href="https://pixar.fandom.com/wiki/Fillmore"> pixar.fandom.com/wiki/Fillmore</a>.</p>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">12 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="../public_html/assets/images/luigi.png" alt="cars luigi">
+                            <div class="caption">
+                                <h4 class="pull-right">1959</h4>
+                                <h4><a href="#">Luigi</a>
+                                </h4>
+                                <p>Luigi runs the local tire shop <a target="_blank" href="https://pixar.fandom.com/wiki/Luigi"> pixar.fandom.com/wiki/Luigi</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">31 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="../public_html/assets/images/mack.png" alt="cars mac">
+                            <div class="caption">
+                                <h4 class="pull-right">Truck</h4>
+                                <h4><a href="#">Mack</a>
+                                </h4>
+                                <p>He's driving Lightning McQueen, the world's fastest racecar. <a target="_blank" href="https://pixar.fandom.com/wiki/Mack"> pixar.fandom.com/wiki/Mac</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">6 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="../public_html/assets/images/mcQueen.png" alt="cars mcqueen">
+                            <div class="caption">
+                                <h4 class="pull-right">95</h4>
+                                <h4><a href="#">McQueen</a>
+                                </h4>
+                                <p>He's driving Lightning McQueen, the world's fastest racecar. <a target="_blank" href="https://pixar.fandom.com/wiki/Lightning_McQueen"> pixar.fandom.com/wiki/Lightning_McQueen</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">99 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="../public_html/assets/images/ramone.png" alt="cars ramonde">
+                            <div class="caption">
+                                <h4 class="pull-right">Artist</h4>
+                                <h4><a href="#">Ramone</a>
+                                </h4>
+                                <p> Ramone's House of Body Art, Radiator Springs. <a target="_blank" href="https://pixar.fandom.com/wiki/Ramone"> pixar.fandom.com/wiki/Ramone</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">48 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="../public_html/assets/images/sallyCarrera.png" alt="cars sally carrera">
+                            <div class="caption">
+                                <h4 class="pull-right">911</h4>
+                                <h4><a href="#">Sally</a>
+                                </h4>
+                                <p> Sally is the town attorney of Radiator Springs. <a target="_blank" href="https://pixar.fandom.com/wiki/Sally_Carrera"> pixar.fandom.com/wiki/Sally_Carrera</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">78 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="../public_html/assets/images/towMater.png" alt="tow mater">
+                            <div class="caption">
+                                <h4 class="pull-right">1951</h4>
+                                <h4><a href="#">Tow Mater</a>
+                                </h4>
+                                <p> Mater's catchphrases are "Dad gum!", "If I'm lyin', I'm cryin'!", and "Git-R-Done!" <a target="_blank" href="https://pixar.fandom.com/wiki/Tow_Mater"> pixar.fandom.com/wiki/Tow_Mater</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">28 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="../public_html/assets/images/thunder.png" alt="chick hicks">
+                            <div class="caption">
+                                <h4 class="pull-right">Thunder</h4>
+                                <h4><a href="#">Chick Hicks</a>
+                                </h4>
+                                <p> Chick Hicks is a racing veteran with a chip on his shoulder. <a target="_blank" href="https://pixar.fandom.com/wiki/Chick_Hicks"> pixar.fandom.com/wiki/Chick_Hicks</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">38 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+
+                    
+    <!-- /.container -->
+<?php include(TEMPLATE_FRONT . DS . "footer.php") ?>
+    
+    
---
+++ resources/templates/front/top_nav.php
@@ -0,0 +1,35 @@
+<div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Digital Actors</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Car Shop</a>
+                    </li>
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+                    <li>
+                        <a href="admin">Chat</a>
+                    </li>
+                     <li>
+                        <a href="checkout.html">Donate</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
